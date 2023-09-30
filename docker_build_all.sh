3d63ea49ab3c08e6242c6ffeb5360e1376e789aa
SUBY <bsa0530@gmail.com>
2023-10-01 00:02:39 +0900

    testa

--- docker_build_all.sh
+++ docker_build_all.sh
@@ -1,8 +1,12 @@
 cd ./gateway
-sudo ./gradlew clean build
+chmod 777 ./gradlew
+./gradlew clean build
 cd ../discovery-server
-sudo ./gradlew clean build
+chmod 777 ./gradlew
+./gradlew clean build
 cd ../config-server
-sudo ./gradlew clean build
+chmod 777 ./gradlew
+./gradlew clean build
 cd ../analysis
-sudo ./gradlew clean build
+chmod 777 ./gradlew
+./gradlew clean build
